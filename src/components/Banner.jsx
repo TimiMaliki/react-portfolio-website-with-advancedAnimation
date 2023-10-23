80d01e79f04fdc13d0db0bad92f8c2b589140343
TimiMaliki <malikitimi@gmail.com>
2023-10-23 16:09:51 +0100

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 
 const Banner = () => {
   return (
    <section className='banner' id='home'>
   <Container>
     <Row className='align-items-center'>
-
+      <Col xs={12} md={6} xl={7}>
+        <span className='tagline'>Welcome to my Portfolio</span>
+        <h1>{`Hi I'm TimiMaliki`}<span className='wrap'></span></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Praesentium dolorum placeat odit!</p>
+            <button onClick={() =>{
+                console.log('lets connect')
+            }}>Let's connect</button>
+      </Col>
     </Row>
   </Container>
    </section>
