9e89726078bbf29d8895eb72a3821481f228adb4
TimiMaliki <malikitimi@gmail.com>
2023-10-23 23:30:13 +0100

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,12 @@
 import { useEffect,useState } from 'react';
 
 import {ArrowRightCircle}  from 'react-bootstrap-icons';
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row, useAccordionButton } from 'react-bootstrap'
 import MeshMello from "../assets/about-me-img/im1.jpg"
 
 const Banner = () => {
     const [loopNum ,setLoopNUm] = useState(0);
-    const [isdelete , setImgDelete] = useState(false);
+    const [isDelete , setIsDelete] = useState(false);
     const  [text, setText] = useState("");
     const  [delta, setDelta] = useState(300 - Math.random() * 100)
     const period = 2000;
@@ -24,7 +24,18 @@ const Banner = () => {
      const tick = () =>{
      let i = loopNum % rotate.length;
      let fullText = rotate[i];
+      let updatedtext = isDelete ? fullText.substring(0, text.length - 1) :  fullText.substring(0, text.length + 1)
 
+      setText(updatedtext)
+
+      if(isDelete && updatedtext === fullText){
+          setIsDelete(true);
+          setDelta(period)
+      }else if(isDelete && updatedtext  === ''){
+                 setIsDelete(false)
+                 setLoopNUm(loopNum + 1)
+                 setDelta(500)
+      }
 
      
      let update 
