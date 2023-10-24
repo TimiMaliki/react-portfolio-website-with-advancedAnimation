694ff20a7ef42c2d27f99c2d9c7414c7b8cd0894
TimiMaliki <malikitimi@gmail.com>
2023-10-24 15:04:40 +0100

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,6 @@ import { Container, Row, Col, Tab, Nav, TabPane } from "react-bootstrap";
 // import { ProjectCard } from "./ProjectCard";
 
 const Projects = () => {
-
   const projects = [
     {
       title: "Business Startup",
@@ -36,43 +35,52 @@ const Projects = () => {
     },
   ];
   return (
-   <section className="project" id="project">
-<Container>
-    <Row>
-        <Col>
-        <h2>Projects</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque accusamus iure nam.</p>
-        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-        <Nav variant="pills" className="flex-column">
-            <Nav.Item>
-              <Nav.Link eventKey="first">Tab 1</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="second">Tab 2</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="third">Tab 3</Nav.Link>
-            </Nav.Item>
-          </Nav>
-          <Tab.Content>
-            <TabPane   eventKey="first">
-          <Row>
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloremque accusamus iure nam.
+            </p>
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <TabPane eventKey="first">
+                  <Row>
+                    {
+                        projects.map((project,index) =>{
+                              return(
+                                <p>{project.title}</p>
+                              )
+                        })
+                    }
+                  </Row>
+                </TabPane>
+                <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
-            
-          </Row>
-
-            </TabPane>
-          </Tab.Content>
-
-
-
-
-          </Tab.Container>
-        </Col>
-    </Row>
-</Container>
-   </section>
-  )
-}
-
-export default Projects
+export default Projects;
