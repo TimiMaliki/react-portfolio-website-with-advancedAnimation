9732e1ec8923c4bb8602e03980c67a718b38400f
TimiMaliki <malikitimi@gmail.com>
2023-10-24 03:00:06 +0100

    Skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbars from './components/Navbars'
 import Banner from './components/Banner';
-
+import Skills from './components/Skills';
 function App() {
 
   return (
     <>
       <Navbars/>
       <Banner/>
+      <Skills/>
     </>
   )
 }
